4c07b937f5914ab9bedbab5d13bdabad6dca26b5
Eduardo Goi Wielens <ewielens@furb.br>
2022-04-25 21:54:09 -0300

    Aula 3

---
+++ Teste/src/Main.java
@@ -0,0 +1,27 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    //Comentário para a linha toda
+/* Comentário de bloco (precisa fechar)
+ble bla bla
+ */
+    static int var1=10;
+    double var2 = 25.8;
+    char var3 = 'a'; //só uma letra
+    String var4 = "abcefg"; //uma cadeia de letras
+    boolean var5 = true;
+
+    public static void main(String[] args) {     //Ctrl + space -> psvm é o atalho)
+        int var1 =10;
+        int var2 =10;
+        System.out.println(var1 != var2);
+
+    }
+
+
+    //private apenas usada por partes do software
+    //protected utilizada apenas pelo pacote que ela está no software
+
+
+}
+
