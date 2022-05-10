1750964aa4247204d2afc7eee6c28ad49c9b7c38
Eduardo Goi Wielens <ewielens@furb.br>
2022-05-09 21:58:41 -0300

    Exercicios

---
+++ Lista1/src/Jogodavelha.java
@@ -0,0 +1,19 @@
+/*import java.util.Scanner;
+
+public class Jogodavelha {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        char[][] matriz = new char[3][3];
+
+        while(true) {
+        }
+        for (int i = 0; i < matriz.length;i++){
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j]=0;
+            }
+            }
+        }
+    }
+    */
+
--- Lista1/src/ListaForString.java
+++ Lista1/src/ListaForString.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ListaForString {
     public static void main(String[] args) {
-        ex03();
+        ex07();
     }
 
     public static void exe01() {
@@ -46,6 +46,45 @@ public class ListaForString {
             System.out.println("Digite um numero");
             soma += in.nextInt();
         }
-        System.out.println("A soma é:"+soma);
+        System.out.println("A soma é:" + soma);
+    }
+
+    public static void ex04() {   //CALCULAR O FATORIAL DE UM NUMERO
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o número que voce deseja saber o fatorial.");
+        int num = in.nextInt();
+        int fator = 1;
+        for (int i = 1; i <= num; i++) {
+            fator *= i;
+        }
+        System.out.println("O fatorial é:" + fator);
+    }
+
+    //Escrever um algoritmo que cheque se o pin informado pelo usuário é válido para ele executar uma ação imaginãria
+    //(eX: ACESSO AO CAIXA ELETRÔNICO), o usuário tem 3 tentativas, o pin tem exatos 5 números.
+    public static void ex06() {
+        Scanner in = new Scanner(System.in);
+        int pin = 91352;
+        for (int i = 2; i >= 0; i--) {
+            System.out.println("Digite a sua senha de acesso.");
+            int user = in.nextInt();
+            if (user == pin) {
+                System.out.println("Ação efetuada");
+            } else {
+                System.out.println("Senha errada, voce tem mais " + i + " tentativas");
+            }
+        }
+    }
+
+    //Escreva um algoritmo que leia um numero e imprima a soma de 1 até este numero
+    public static void ex07() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o número");
+        int num = in.nextInt();
+        int soma = 0;
+        for (int i = 1; i <= num; i++) {
+            soma += i;
+        }
+        System.out.print("A soma é:" +soma);
     }
 }
